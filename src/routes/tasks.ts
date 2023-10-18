b7a21bb43144d51e53e993891be3ff31fed904c5
Patrick Cruz <patrickezequielscruz@gmail.com>
2023-10-18 20:26:37 +0200

    refactoring adding a try catch

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -9,42 +9,51 @@ const connectDB = async () => {
 }
 
 router.get('/', async (req: any, res: any) => {
-    const tasks = await connectDB()
-    console.log(`connection made`);
-    console.log(`start getTasks`);
-     try {
-        const allTasks =  await tasks.getTasks()
+    try {
+        const tasks = await connectDB()
+        const allTasks = await tasks.getTasks()
         return res.json(allTasks)
-     } catch (error) {
-        console.log(`ERROR `, error)
-        return error
-     }
+    } catch (error) {
+        console.error('Error handling GET request:', error);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
 })
 
 router.post('/', async (req: any, res: any) => {
-    const tasks = await connectDB()
-    return res.json(tasks.create(req.body))
+    try {
+        const tasks = await connectDB();
+        const result = await tasks.create(req.body);
+        return res.json(result);
+    } catch (error) {
+        console.error('Error handling POST request:', error);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
 })
 
 router.delete('/:id', async (req: any, res: any) => {
-    const taskId = req.params.id // Access the id from the URL parameters
-    const tasks = await connectDB()
-    const result = await tasks.deleteOne(taskId)
-    res.json(result)
+    try {
+        const taskId = req.params.id;
+        const tasks = await connectDB();
+        const result = await tasks.deleteOne(taskId);
+        res.json(result);
+    } catch (error) {
+        console.error('Error handling DELETE request:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
 })
 
 router.put('/:id', async (req: any, res: any) => {
-    const tasks = await connectDB()
     try {
-        const result = await tasks.updateOne(req)
+        const tasks = await connectDB();
+        const result = await tasks.updateOne(req);
         if (result) {
-            res.json(result)
+            res.json(result);
         } else {
-            res.status(404).json({ message: 'Task not found' })
+            res.status(404).json({ message: 'Task not found' });
         }
     } catch (error) {
-        console.error('Error updating task:', error)
-        res.status(500).json({ message: 'Internal Server Error' })
+        console.error('Error handling PUT request:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
     }
 })
 
