ae1ec20b19db0197fea03950059a453b07456dd6
Patrick Cruz <patrickezequielscruz@gmail.com>
2023-10-18 20:05:53 +0200

    adding try and catch

--- src/controller/tasks.ts
+++ src/controller/tasks.ts
@@ -28,8 +28,8 @@ class Tasks {
         try {
             const options = {
                 // Other connection options...
-                bufferCommands: false, // Set to false to disable command buffering
-                bufferTimeoutMS: 50000, // Set the desired timeout value in milliseconds
+                poolSize: 10,       // Set the maximum number of connections in the pool
+                maxPoolSize: 10,    // Set the maximum number of connections in the pool
             };
             await mongoose.connect(uri, options)
         } catch (error) {
@@ -39,44 +39,65 @@ class Tasks {
     }
 
     async getTasks() {
-        const tasks = await TaskModel.find({}).lean().exec()
-        console.log(`value of tasks `, tasks);
-        return tasks
+        try {
+            const tasks = await TaskModel.find({}).lean().exec()
+            console.log(`value of tasks `, tasks);
+            return tasks
+        } catch (error) {
+            console.log(error)
+            throw error
+        }
     }
 
     async create(task: ITask) {
-        const result = await TaskModel.create(task)
-        console.log(result)
-        return 'worked'
+        try {
+            const result = await TaskModel.create(task)
+            console.log(result)
+            return 'worked'
+        }
+        catch (error) {
+            console.log(error)
+            throw error
+        }
     }
 
     async updateOne(req: any) {
-        const taskId = req.params.id
-        const update: any = {}
+        try {
+            const taskId = req.params.id
+            const update: any = {}
 
-        if (req.body.title) {
-            update.title = req.body.title
+            if (req.body.title) {
+                update.title = req.body.title
+            }
+
+            if (req.body.description) {
+                update.description = req.body.description
+            }
+
+            const updatedTask = await TaskModel.findOneAndUpdate(
+                { _id: taskId },
+                { $set: update },
+                { new: true }
+            )
+
+            return updatedTask
+        } catch (error) {
+            console.log(error)
+            throw error
         }
-
-        if (req.body.description) {
-            update.description = req.body.description
-        }
-
-        const updatedTask = await TaskModel.findOneAndUpdate(
-            { _id: taskId },
-            { $set: update },
-            { new: true }
-        )
-
-        return updatedTask
     }
 
     async deleteOne(id: string) {
-        const result = await TaskModel.deleteOne({ _id: id })
-        if (result.deletedCount === 1) {
-            return 'Document deleted successfully'
-        } else {
-            return 'Document not found or not deleted'
+        try {
+            const result = await TaskModel.deleteOne({ _id: id })
+            if (result.deletedCount === 1) {
+                return 'Document deleted successfully'
+            } else {
+                return 'Document not found or not deleted'
+            }
+        } catch (error) {
+            console.log(error)
+            throw error
         }
     }
 }
