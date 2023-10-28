2722c91a871da5ea5274f006c5be226764326a51
Patrick Cruz <patrickezequielscruz@gmail.com>
2023-10-28 16:05:10 +0200

    allowing CORS

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,15 @@ import express from 'express'
 import routes from './routes'
 
 const app = express()
+
+// Allow requests from any origin
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+  
 app.use(express.json())
 app.use(routes)
 
